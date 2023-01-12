383ce829cc9797e291c47b400b6ffff3434a1c43
Advaito <108537156+Advaito@users.noreply.github.com>
2023-01-12 19:16:31 +0300

    v1.01

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,13 +6,14 @@ import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
 @Component
 public class User {
-
+    @NotNull
     private Long id;
 
     @Email
