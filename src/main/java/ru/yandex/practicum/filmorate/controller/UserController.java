29f3ea40e1baca8c1ad7c10fa5f543722d5b284e
huuudrich <108597096+huuudrich@users.noreply.github.com>
2023-03-15 21:47:24 +1100

    feature
    feature: Добавил возможность удаление фильма и пользователя по id

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -69,7 +69,8 @@ public class UserController {
 
     @DeleteMapping("/users/{userId}")
     public void deleteUserById(@PathVariable Long userId) {
-
+        log.info("Удален пользователь id = {}", userId);
+        userService.deleteUserById(userId);
     }
 
 }
