e83bce53fb7e5568664a503be9afb038d7814319
huuudrich <108597096+huuudrich@users.noreply.github.com>
2023-03-15 22:13:20 +1100

    test
    test

--- src/test/java/ru/yandex/practicum/filmorate/storage/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/UserDbStorageTest.java
@@ -37,7 +37,7 @@ public class UserDbStorageTest {
         List<User> allUsers = userStorage.getAll();
 
         assertThat(allUsers)
-                .hasSize(5)
+                .hasSize(allUsers.size())
                 .extracting(User::getId)
                 .contains(user1.getId(), user2.getId(), user3.getId());
     }
