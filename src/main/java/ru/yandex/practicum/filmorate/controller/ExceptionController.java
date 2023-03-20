20a87fc9eb08746ef9560a11337916d23f06f538
Eugene Pelipets <eugenepelipets@gmail.com>
2023-03-20 21:12:29 +0300

    Fix

--- src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.webjars.NotFoundException;
 
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
